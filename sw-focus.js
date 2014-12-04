6426335470c0b077001e601df7693401f4cdc1aa
Mounir Lamouri <mounir@lamouri.fr>
2014-12-04 19:15:54 +0000

    Update sw-focus.js

--- sw-focus.js
+++ sw-focus.js
@@ -4,10 +4,10 @@ self.onmessage = function(event) {
   console.log('message received ' + event);
   
   event.waitUntil(
-    self.clients.getAll().then(function(c) {
-      console.log('trying to focus');
-      return c[0].focus();
-    });
+//    self.clients.getAll().then(function(c) {
+//      console.log('trying to focus');
+//      return c[0].focus();
+//    });
   );
 };
 
